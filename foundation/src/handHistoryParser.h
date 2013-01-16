5423d83a728c47b8a426ef21259d85bcc2a50683
Matyas Matyas <duplamayi@gmail.com>
2013-01-17 01:16:04 +0200

    Fixed updater in debug and release modes.

--- foundation/src/handHistoryParser.h
+++ foundation/src/handHistoryParser.h
@@ -11,5 +11,5 @@ class HandHistoryParser
 {
 public:
 	virtual vector<HandHistory> parse(string filename) = 0;
-    virtual ~HandHistoryParser() = 0;
+    //TODO: Research on virtual constructors. Do not declare it unless you know exactly what are you doing.
 };
